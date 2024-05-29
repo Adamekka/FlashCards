82982f86e20e69717b2bb2f93bea658f73a04f70
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2024-05-29 16:13:47 +0200

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Adam Cvikl on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
